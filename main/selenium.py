41f3de483cedad1380e5bf3565f927bd76f877da
Lemon Ads <lemonads@protonmail.com>
2020-01-10 13:26:11 +0545

    added selenium

--- main/selenium.py
+++ main/selenium.py
@@ -6,9 +6,14 @@ import os
 
 def replaceKey(key):
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    driver_options = Options()
+    
+    # import tweaks for deployment with heroku
+    driver_options = webdriver.ChromeOptions()
+    driver_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     driver_options.add_argument("--headless")
-    driver = webdriver.Chrome(dir_path + "/driver/chromedriver", options=driver_options)
+    driver_options.add_argument("--disable-dev-shm-usage")
+    driver_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(os.environ.get("CHROMEDRIVER_PATH"), options=driver_options)
     driver.get("https://supgrade.eu/api?method=replace&key="+key)
     sleep(5)
     response = driver.find_element_by_tag_name("body").get_attribute("innerHTML")
